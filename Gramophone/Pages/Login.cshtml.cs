cf43aefee7311bacd06b96d7b89e4e6c6f8a2027
bulat <madent0045@gmail.com>
2022-10-12 20:29:24 +0300

    added sign in and log out (need to fix html)

--- Gramophone/Pages/Login.cshtml.cs
+++ Gramophone/Pages/Login.cshtml.cs
@@ -1,10 +1,47 @@
+using Gramophone.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace Gramophone.Pages
 {
+    [BindProperties]
     public class LoginModel : PageModel
     {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        [Required(ErrorMessage = "Поле не может быть пустым")]
+        [Display(Name = "Имя пользователя")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Поле не может быть пустым")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Name, Password, true, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToPage("/Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("Password", "Неправильный логин или пароль");
+                    return Page();
+                }
+            }
+            else
+            { return Page(); }
+        }
         public void OnGet()
         {
         }
