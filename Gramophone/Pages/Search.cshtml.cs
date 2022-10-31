9b329824c68e250d5c3c58bd2538a3ad101b87f6
bulat <madent0045@gmail.com>
2022-10-31 19:20:58 +0300

    added search functionality (only local compositions)

--- Gramophone/Pages/Search.cshtml.cs
+++ Gramophone/Pages/Search.cshtml.cs
@@ -1,12 +1,24 @@
+using Gramophone.Data;
+using Gramophone.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gramophone.Pages
 {
+    [BindProperties(SupportsGet = true)]
     public class SearchModel : PageModel
     {
-        public string SearchString { get; set; }
-        public void OnGet()
+        public string? SearchString { get; set; }
+        public List<Composition>? Compositions { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            CompositionsExample temp = new CompositionsExample();
+            Compositions = temp.Compositions;
+        }
+
+        public void OnPost()
         {
         }
     }
