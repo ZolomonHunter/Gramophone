b700aed9121cc9309773d06e86fa519b6777afd7
bulat <madent0045@gmail.com>
2022-11-02 08:12:00 +0300

    reworked Search to get compositions from db

--- Gramophone/Pages/Search.cshtml.cs
+++ Gramophone/Pages/Search.cshtml.cs
@@ -9,14 +9,25 @@ namespace Gramophone.Pages
     [BindProperties(SupportsGet = true)]
     public class SearchModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
         public string? SearchString { get; set; }
         public List<Composition>? Compositions { get; set; }
-
+        public SearchModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public async Task OnGetAsync()
         {
             // TODO take from db and sort here instead in view
-            CompositionsExample temp = new CompositionsExample();
-            Compositions = temp.Compositions;
+            /*CompositionsExample temp = new CompositionsExample();
+            Compositions = temp.Compositions;*/
+            if (String.IsNullOrEmpty(SearchString))
+                Compositions = _db.Compositions.ToList();
+            else{
+                Compositions = _db.Compositions.ToList().Where(
+                (p => p.Name.ToLower().Contains(SearchString.ToLower()) || p.Author.ToLower().Contains(SearchString.ToLower())
+                )).ToList();
+            }
         }
 
         public void OnPost()
